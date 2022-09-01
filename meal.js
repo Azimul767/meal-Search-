7039059d673baa553a8a4fb28c09d2ccdccad644
Azimul767 <75627038+Azimul767@users.noreply.github.com>
2022-09-01 13:21:49 +0600

    first commit

---
+++ meal.js
@@ -0,0 +1,35 @@
+const loadmeal = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch (url)
+    .then (response => response.json ())
+    .then (data => displaymeals(data.meals) )
+}
+
+const displaymeals = meals => {
+   const mealconatiner = document.getElementById('meal-container')
+   mealconatiner.innerHTML = ``;
+   meals.forEach(meal => {
+   const mealDiv = document.createElement('div')
+   mealDiv.classList.add('col')
+   mealDiv.innerHTML = `
+   <div class="card">
+   <img src="${meal.strMealThumb} " class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title"> ${meal.strMeal} </h5>
+        <p class="card-text">${meal.strInstructions.slice(0,200)} </p>
+    </div>
+ </div>
+   `
+   mealconatiner.appendChild(mealDiv)
+
+   });
+}
+
+const searchFood = () => {
+    const searchFild = document.getElementById('search-fild');
+    const searchvalue = searchFild.value;
+    loadmeal(searchvalue);
+    searchFild.value = '';
+}
+
+// loadmeal('a');
